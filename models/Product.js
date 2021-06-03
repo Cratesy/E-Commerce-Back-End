27be86c9d8804514e32c544504abafc4b7c43272
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-06-03 17:41:16 +0100

    added schema and options for products

--- models/Product.js
+++ models/Product.js
@@ -1,23 +1,55 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    validate: {
+      decimal: true,
+    },
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    default: 10,
+    allowNull: false,
+    validate: {
+      INTEGER: true,
+    },
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    References: {
+      model: "Category",
+      key: "id",
+    },
+  },
+};
+
+const options = {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "product",
+};
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
 // set up fields and rules for Product model
-Product.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
-);
+Product.init(schema, options);
 
 module.exports = Product;
