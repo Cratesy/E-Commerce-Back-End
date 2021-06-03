1c60596a186eead0e710ce1207296703e083ce61
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-06-03 17:48:35 +0100

    done schema and options for tag

--- models/Tag.js
+++ models/Tag.js
@@ -2,19 +2,29 @@ const { Model, DataTypes } = require("sequelize");
 
 const sequelize = require("../config/connection.js");
 
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  tag_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+};
+
+const options = {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "tag",
+};
+
 class Tag extends Model {}
 
-Tag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "tag",
-  }
-);
+Tag.init(schema, options);
 
 module.exports = Tag;
