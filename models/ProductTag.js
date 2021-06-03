40c7521a124bea466cf1fce2e7b9e8e5aa41bd01
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-06-03 17:46:31 +0100

    added schema and options for productTag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,42 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
+
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  product_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "Product",
+      key: "id",
+    },
+  },
+  tag_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "Tag",
+      key: "id",
+    },
+  },
+};
+
+const options = {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "product_tag",
+};
 
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
-);
+ProductTag.init(schema, options);
 
 module.exports = ProductTag;
